6d1a0fcd01c34f5ab023b367a5f77fa83e67fb85
Efe Beray <134969960+efeberay@users.noreply.github.com>
2026-02-25 18:10:34 +0300

    Add files via upload

---
+++ PizzaSiparişEkranı/PizzaSiparişEkranı/Form1.Designer.cs
@@ -0,0 +1,403 @@
+﻿namespace PizzaSiparişEkranı
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            asdToolStripMenuItem = new ToolStripMenuItem();
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            label3 = new Label();
+            listBox1 = new ListBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            groupBox1 = new GroupBox();
+            radioButton8 = new RadioButton();
+            radioButton9 = new RadioButton();
+            radioButton10 = new RadioButton();
+            radioButton11 = new RadioButton();
+            radioButton12 = new RadioButton();
+            radioButton7 = new RadioButton();
+            radioButton6 = new RadioButton();
+            radioButton5 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton3 = new RadioButton();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            contextMenuStrip1.SuspendLayout();
+            panel1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { asdToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(93, 26);
+            // 
+            // asdToolStripMenuItem
+            // 
+            asdToolStripMenuItem.Name = "asdToolStripMenuItem";
+            asdToolStripMenuItem.Size = new Size(92, 22);
+            asdToolStripMenuItem.Text = "asd";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Maroon;
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(469, 69);
+            panel1.TabIndex = 1;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F);
+            label1.Location = new Point(150, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(194, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Pizza Sipariş Ekranı";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(73, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(36, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Ebat :";
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Küçük", "Orta", "Büyük" });
+            comboBox1.Location = new Point(144, 93);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(217, 23);
+            comboBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(31, 148);
+            label3.Name = "label3";
+            label3.Size = new Size(84, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Pizza Çeşitleri :";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Items.AddRange(new object[] { "American Hot", "Calypso", "Turkish", "Pizza Italiano", "Extravaganzza" });
+            listBox1.Location = new Point(144, 148);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(217, 79);
+            listBox1.TabIndex = 6;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(144, 233);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(80, 19);
+            radioButton1.TabIndex = 7;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "İnce Kenar";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(277, 233);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(84, 19);
+            radioButton2.TabIndex = 8;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Kalın Kenar";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton8);
+            groupBox1.Controls.Add(radioButton9);
+            groupBox1.Controls.Add(radioButton10);
+            groupBox1.Controls.Add(radioButton11);
+            groupBox1.Controls.Add(radioButton12);
+            groupBox1.Controls.Add(radioButton7);
+            groupBox1.Controls.Add(radioButton6);
+            groupBox1.Controls.Add(radioButton5);
+            groupBox1.Controls.Add(radioButton4);
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Location = new Point(144, 278);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(217, 151);
+            groupBox1.TabIndex = 9;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Malzemeler";
+            // 
+            // radioButton8
+            // 
+            radioButton8.AutoSize = true;
+            radioButton8.Location = new Point(133, 122);
+            radioButton8.Name = "radioButton8";
+            radioButton8.Size = new Size(57, 19);
+            radioButton8.TabIndex = 9;
+            radioButton8.TabStop = true;
+            radioButton8.Text = "Sucuk";
+            radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            radioButton9.AutoSize = true;
+            radioButton9.Location = new Point(133, 97);
+            radioButton9.Name = "radioButton9";
+            radioButton9.Size = new Size(57, 19);
+            radioButton9.TabIndex = 8;
+            radioButton9.TabStop = true;
+            radioButton9.Text = "Salam";
+            radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            radioButton10.AutoSize = true;
+            radioButton10.Location = new Point(133, 72);
+            radioButton10.Name = "radioButton10";
+            radioButton10.Size = new Size(64, 19);
+            radioButton10.TabIndex = 7;
+            radioButton10.TabStop = true;
+            radioButton10.Text = "Ançuez";
+            radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // radioButton11
+            // 
+            radioButton11.AutoSize = true;
+            radioButton11.Location = new Point(133, 47);
+            radioButton11.Name = "radioButton11";
+            radioButton11.Size = new Size(51, 19);
+            radioButton11.TabIndex = 6;
+            radioButton11.TabStop = true;
+            radioButton11.Text = "Mısır";
+            radioButton11.UseVisualStyleBackColor = true;
+            // 
+            // radioButton12
+            // 
+            radioButton12.AutoSize = true;
+            radioButton12.Location = new Point(133, 22);
+            radioButton12.Name = "radioButton12";
+            radioButton12.Size = new Size(51, 19);
+            radioButton12.TabIndex = 5;
+            radioButton12.TabStop = true;
+            radioButton12.Text = "Sosis";
+            radioButton12.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            radioButton7.AutoSize = true;
+            radioButton7.Location = new Point(6, 122);
+            radioButton7.Name = "radioButton7";
+            radioButton7.Size = new Size(58, 19);
+            radioButton7.TabIndex = 4;
+            radioButton7.TabStop = true;
+            radioButton7.Text = "Peynir";
+            radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            radioButton6.AutoSize = true;
+            radioButton6.Location = new Point(6, 97);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(77, 19);
+            radioButton6.TabIndex = 3;
+            radioButton6.TabStop = true;
+            radioButton6.Text = "Ton Balığı";
+            radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(6, 72);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(63, 19);
+            radioButton5.TabIndex = 2;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "Mantar";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(6, 47);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(58, 19);
+            radioButton4.TabIndex = 1;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Zeytin";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(6, 22);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(97, 19);
+            radioButton3.TabIndex = 0;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Dana Jambon";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(143, 450);
+            label4.Name = "label4";
+            label4.Size = new Size(32, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Adet";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(268, 450);
+            label5.Name = "label5";
+            label5.Size = new Size(47, 15);
+            label5.TabIndex = 11;
+            label5.Text = "Toplam";
+            label5.Click += label5_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(187, 447);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(40, 23);
+            textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(321, 447);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(40, 23);
+            textBox2.TabIndex = 13;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(171, 480);
+            button1.Name = "button1";
+            button1.Size = new Size(92, 30);
+            button1.TabIndex = 14;
+            button1.Text = "Kapat";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(269, 480);
+            button2.Name = "button2";
+            button2.Size = new Size(92, 30);
+            button2.TabIndex = 15;
+            button2.Text = "Sipariş Ver";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(469, 546);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(groupBox1);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(listBox1);
+            Controls.Add(label3);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "PIZZA";
+            contextMenuStrip1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem asdToolStripMenuItem;
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private Label label3;
+        private ListBox listBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private GroupBox groupBox1;
+        private RadioButton radioButton8;
+        private RadioButton radioButton9;
+        private RadioButton radioButton10;
+        private RadioButton radioButton11;
+        private RadioButton radioButton12;
+        private RadioButton radioButton7;
+        private RadioButton radioButton6;
+        private RadioButton radioButton5;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ PizzaSiparişEkranı/PizzaSiparişEkranı/Form1.cs
@@ -0,0 +1,31 @@
+namespace PizzaSiparişEkranı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // "Kapat" butonuna basıldığında formu kapat
+            this.Close();
+        }
+    }
+}
